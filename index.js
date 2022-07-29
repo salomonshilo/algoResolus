2f9ba67f966df7347ff3b9f077b591b67166d4b8
salomonshilo <shilomavonga@mail.com>
2022-07-29 14:34:54 +0100

    Ajout d'un algo

--- index.js
+++ index.js
@@ -1,49 +1,11 @@
+// algo sqrt
 
-// //algo two sums
+var mySqrt = function(x) {
+    
+    const exp = 0.5 * Math.log10(x);
+    return Math.floor(10 ** exp);
 
-// var twoSum = function(nums, target) {
-
-//     for(i = 0 ; i< nums.length ; i++){
-//         for(let j = i+1; j < nums.length ; j ++){
-//             if(nums[i] + nums[j] == target){
-//                 return [i,j]
-           
-            
-//             }
-//         }
-//     } 
-
-// };
-// nums = [2,7,11,15];
-// target = 9
-
-// console.log(twoSum(nums,target));
-
-
-// algo anagrame
-
-var anagram = function ( s,t){
-
-    let order1 = s.split("").sort( ).join("");
-
-    let order2 = t.split("").sort( ).join("");
-
-    if(order1 == order2){
-        return true;
-    }
-    else{
-        return false;
-    }
 };
 
-s = 'rac'
-t= 'car'
-console.log(anagram(s,t));
-
-
-
-
-
-
-
-
+x = 144
+console.log(mySqrt(x));
