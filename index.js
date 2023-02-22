7fce7159fba0f6f3b17c566ff0e287ea845e26a3
salomonshilo <shilomavonga@mail.com>
2023-02-22 16:15:13 +0100

    feature: modifaction multication

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-// algo sqrt
+// var numbers = [1, 2, 3, 4];
+//  var new_numbers = [];
+//  for(var i = 0; i < numbers.length; i++) {
+//  new_numbers[i] = numbers[i] * 2;
+//  }
+//  console.log("Les nombres doublés sont", new_numbers); // [2, 4, 6, 8]
 
-var mySqrt = function(x) {
-    
-    const exp = 0.5 * Math.log10(x);
-    return Math.floor(10 ** exp);
 
-};
+// var numbers = [1,2,3,4];
 
-x = 144
-console.log(mySqrt(x));
+
+// var new_numbers = numbers.map(number => number * 2)
+
+// console.log(new_numbers);
+
+
+// var tab = [1,2,3,4];
+
+// var news_tab = [];
+
+// for(let i = 0 ; i < tab.length ; i++){
+// if(tab[i] > 2){
+//       news_tab[i] = tab[i]
+// }
+// }
+// console.log(news_tab);
+
+// let tab = [1,2,3,4];
+
+// let new_tab = tab.filter(function(number){
+//     return number > 0;
+// });
+
+// console.log(new_tab);
+
+
+
+
+var numbers = [1, 2, 3, 4];
+ var total_number = numbers.map(function(number){
+ return number * 3;
+ }).reduce(function(total, number){return total + number;}, 0);
+console.log("Le total est", total_number);
+ 
+
